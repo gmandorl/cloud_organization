6eae9ba8a290676116c922d273a8a9d72c6dabec
gmandorl <giulio.mandorli@lmd.ipsl.fr>
2021-07-27 22:42:33 +0200

    region objects are moved to Pairs in objects.py. Several other region properties are added. Moreover, the metrics are saved in a txt file

--- run_metrics.py
+++ run_metrics.py
@@ -1,18 +1,8 @@
 import xarray as xr
 import numpy as np
 import datetime
-#import functools
-#import math as m
 import timeit
-import skimage.measure as skm
-#import scipy as sp
-import shapely.geometry as spg
-import shapely.ops as spo
 
-
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from objects import Pairs
 import metrics
@@ -20,63 +10,11 @@ import metrics
 
 
 
-def gen_shortlist(start, inlist):
-    """Iterator items starting at 'start', not 0."""
-    for j in range(start, len(inlist)):
-        yield inlist[j]
-
-
-def gen_tuplelist(inlist):
-    """Tuples of all possible unique pairs in an iterator. For one element only in iterator, yields tuple with two
-    times the same item."""
-    #print ("\ninlist \n ", inlist , "\n\n")
-    if len(inlist) == 1:
-        yield inlist[0], inlist[0]
-    else:
-        for i, item1 in enumerate(inlist):
-            #print ("inlist.area ", item1.area )
-            for item2 in gen_shortlist(start=i + 1, inlist=inlist):
-                yield item1, item2
-
-
-def get_objects(sky_scene) :
-
-    labeled = skm.label(sky_scene, background=0)  # , connectivity=1)
-    objects = skm.regionprops(labeled)
-
-    #ax = plt.subplot(111)
-    #im = ax.imshow(np.flipud(labeled))
-    #divider = make_axes_locatable(ax)
-    #cax = divider.append_axes("right", size="5%", pad=0.05)
-    #plt.colorbar(im, cax=cax), plt.show()
-
-    polys = []
-    for o in objects:
-        layout = o.image.astype(int)
-        bounds = o.bbox
-        y_length = bounds[2] - bounds[0]
-        x_length = bounds[3] - bounds[1]
-        # prepare bed to put layout in
-        bed = np.zeros(shape=(y_length + 2, x_length + 2))
-        bed[1:-1, 1:-1] = layout
-        # get the contour needed for shapely
-        contour = skm.find_contours(bed, level=0.5, fully_connected='high')
-        # increase coordinates to get placement inside of original input array right
-        contour[0][:, 0] += bounds[0]  # increase y-values
-        contour[0][:, 1] += bounds[1]  # increase x-values
-        # sugar coating needed for shapely (contour only consists of 1 object...anyways)
-        coordinates = [tuple(c)  for c in contour[0]]
-
-        m_poly = spg.Polygon(coordinates)
-        #print(m_poly.centroid, m_poly.area, m_poly.length, "\n\n")
-        polys.append(m_poly)
-
-
-    return objects, polys
-
 
 
 def run_metrics( file_name="") :
+
+
 
     excl_var= ['EPScld', 'Pcld', 'frac_Rain', 'frac_Lay', 'Psurf', 'Tsurf', 'frac_mlow', 'DZ_mlow', 'DZtopCIRS_mlow', 'Ztop_mlow', 'RR_mlow', 'Ptropo', 'cldtyp', 'frac_cldtyp', 'Ptropo', 'RR_Cb', 'RR_Ci', 'RR_thCi', 'DZ_Cb', 'DZ_Ci', 'DZ_thCi', 'DZtopCIRS_Cb', 'DZtopCIRS_Ci', 'DZtopCIRS_thCi', 'EPS_scen', 'MultiLayer', 'P_scen', 'Ztop_Cb', 'Ztop_Ci', 'Ztop_thCi', 'cldtypinHL', 'count_cld', 'frac_Cb', 'frac_Ci', 'frac_cldtypinHL', 'frac_scen', 'frac_thCi', 'landfrac']
     ds = xr.open_dataset(file_name, decode_times=False, drop_variables=excl_var)
@@ -85,7 +23,8 @@ def run_metrics( file_name="") :
     sky_scene = np.nan_to_num(sky_scene, nan=0.)
     total_number_of_pixels = sky_scene.shape[0]*sky_scene.shape[1]
 
-    list_regions, list_polys = get_objects(sky_scene)
+    #list_regions, list_polys = get_objects(sky_scene)
+
 
 
     print ("total_number_of_pixels : ", sky_scene.shape[0], sky_scene.shape[1], total_number_of_pixels)
@@ -93,23 +32,53 @@ def run_metrics( file_name="") :
     #print ("list_polys : ", len(list_polys) ,"\n", list_polys)
 
     #all_s_pairs = [Pairs(pairlist=list(gen_tuplelist(cloudlist))) for cloudlist in props_s]
-    all_pairs = Pairs(region_pairs=list(gen_tuplelist(list_regions)), poly_pairs=list(gen_tuplelist(list_polys)))
+    #all_pairs = Pairs(region_pairs=list(gen_tuplelist(list_regions)), poly_pairs=list(gen_tuplelist(list_polys)))
+    all_pairs = Pairs(sky_scene)
 
-    #print ("all_pairs : ", all_pairs)
+    #------------------ METRICS ---------------------
 
-    #iorg = metrics.i_org(pairs=all_pairs, objects=list_regions, total_number_of_pixels = total_number_of_pixels)
-    iorg = metrics.i_org(pairs=all_pairs, objects=list_regions, total_number_of_pixels = 9841.)
+    #iorg    = metrics.i_org(all_pairs, total_number_of_pixels = 9841.)
+    iorg    = metrics.i_org(all_pairs, total_number_of_pixels)
+    rom     = 6.25 * metrics.radar_organisation_metric(all_pairs)
+    rom_el  = 6.25 * metrics.elliptic_shape_organisation(all_pairs, elliptic = True)
+    scai    = metrics.simple_convective_aggregation_metric(all_pairs, total_number_of_pixels)
+    cop     = metrics.conv_org_pot(all_pairs)
 
-    rom = 6.25 * metrics.radar_organisation_metric(all_pairs)
+    #-------------- OTHER PROPERTIES ----------------
 
+    cloud_number = all_pairs.get_number_of_regions()
+    total_area = np.array([c.area for c in all_pairs.list_regions]).sum()
+    cloud_max_area = np.array([c.area for c in all_pairs.list_regions]).max()
 
-    fileTXT = open("txt_files/info_"+str(datetime.datetime.now().strftime('%y%m%d_%H%M%S'))+".txt","w")
+    fileTXT = open("txt_files3/info_"+str(datetime.datetime.now().strftime('%y%m%d_%H%M%S'))+".txt","w")
     fileTXT.write("file name: \t" + file_name + "\n")
     fileTXT.write("rom: \t" + str(rom) + "\n")
-    fileTXT.write("iorg: \t" + str(iorg) + "\n\n")
+    fileTXT.write("iorg: \t" + str(iorg) + "\n")
+    fileTXT.write("COP: \t" + str(cop) + "\n")
+    fileTXT.write("SCAI: \t" + str(scai) + "\n\n")
     fileTXT.close()
 
+    #print("metrics: ", iorg, rom, rom_el, scai, cop, cloud_number, total_area, cloud_max_area)
 
+    ds_m = xr.Dataset({'cop':       (("time"), np.ones(1)*cop),
+                       'rom_el':    (("time"), np.ones(1)*rom_el),
+                       'rom':       (("time"), np.ones(1)*rom),
+                       'iorg':      (("time"), np.ones(1)*iorg),
+                       'scai':      (("time"), np.ones(1)*scai),
+                       #'rom_el': rom_el,
+                       #'rom': rom,
+                       #'iorg': iorg,
+                       #'scai': scai,
+                       'cloud_number':      (("time"), np.ones(1)*cloud_number),
+                       'total_area':        (("time"), np.ones(1)*total_area),
+                       'cloud_max_area':    (("time"), np.ones(1)*cloud_max_area),
+                       },
+                     #coords={"time": [datetime.datetime(2008, 1, 3)],}
+                     coords={"time": [datetime.datetime.now()],}
+    )
+
+
+    return ds_m
 
 
 
